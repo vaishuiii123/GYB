6b6b76c78f15f5fd2284c6686271724c4b2f6886
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-27 12:45:07 +0530

    Refactor main.tsx for improved readability and structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 import {
   HashRouter,
   Routes,
   Route,
 } from "react-router-dom";
 
-import './index.css'
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Organization from "./pages/Organization";
+import Template from "./pages/Template";
+import Workshop from "./pages/Workshop";
 
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={<Login />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
+        <Route
+          path="/organization"
+          element={<Organization />}
+        />
+
+        <Route
+          path="/template"
+          element={<Template />}
+        />
+
+        <Route
+          path="/workshop"
+          element={<Workshop />}
+        />
       </Routes>
     </HashRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
