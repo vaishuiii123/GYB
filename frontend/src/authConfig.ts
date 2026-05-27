912a46c5d8bd284048976bd8fc1ad65fda6b9066
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-27 20:07:19 +0530

    Create authConfig.ts

---
+++ frontend/src/authConfig.ts
@@ -0,0 +1,12 @@
+export const msalConfig = {
+  auth: {
+    clientId: "e14af128-d6f9-463f-9275-e1cdfea7728a",
+    authority: "https://login.microsoftonline.com/8e108e06-7848-48cf-8623-e6b06c27f2db",
+    redirectUri: "https://gentle-sea-0636fbe10.7.azurestaticapps.net"
+  }
+};
+
+export const loginRequest = {
+  scopes: ["User.Read"]
+};
+``
