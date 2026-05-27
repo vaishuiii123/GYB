69d58fc329663d9cddc37f99a7732190578fe5c6
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-27 20:10:50 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,51 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import {
-  HashRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./authConfig";
 
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Organization from "./pages/Organization";
-import Template from "./pages/Template";
-import Workshop from "./pages/Workshop";
+const msalInstance = new PublicClientApplication(msalConfig);
 
-import "./index.css";
-
-ReactDOM.createRoot(
-  document.getElementById("root")!
-).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Login />}
-        />
-
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-
-        <Route
-          path="/organization"
-          element={<Organization />}
-        />
-
-        <Route
-          path="/template"
-          element={<Template />}
-        />
-
-        <Route
-          path="/workshop"
-          element={<Workshop />}
-        />
-      </Routes>
-    </HashRouter>
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
