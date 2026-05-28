0e93dd117856437d432e560552b181228f1f641f
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-28 13:12:27 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 import { PublicClientApplication } from "@azure/msal-browser";
-
 import { MsalProvider } from "@azure/msal-react";
-
-import App from "./App";
 
 import { msalConfig } from "./authConfig";
 
 import "./index.css";
 
-const msalInstance =
-  new PublicClientApplication(msalConfig);
+const msalInstance = new PublicClientApplication(msalConfig);
 
 msalInstance.initialize().then(() => {
-  ReactDOM.createRoot(
-    document.getElementById("root")!
-  ).render(
-    <React.StrictMode>
-      <MsalProvider instance={msalInstance}>
-        <App />
-      </MsalProvider>
-    </React.StrictMode>
-  );
+  msalInstance.handleRedirectPromise().then(() => {
+    ReactDOM.createRoot(
+      document.getElementById("root") as HTMLElement
+    ).render(
+      <React.StrictMode>
+        <MsalProvider instance={msalInstance}>
+          <App />
+        </MsalProvider>
+      </React.StrictMode>
+    );
+  });
 });
