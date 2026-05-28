079d7ed87a06983e3000c943c64d65d4e152fb05
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-28 16:48:22 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "./authConfig";
 
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 import {
   HashRouter,
   Routes,
@@ -8,7 +10,6 @@ import {
   Navigate,
 } from "react-router-dom";
 
-import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Organization from "./pages/Organization";
 import Template from "./pages/Template";
@@ -28,45 +29,17 @@ function App() {
     }
   };
 
-  // SHOW LOGIN PAGE
+ 
 
   if (accounts.length === 0) {
     return <Login onLogin={handleLogin} />;
   }
 
-  // SHOW APPLICATION
-
   return (
-    <HashRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Navigate to="/dashboard" />
-          }
-        />
-
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-
-        <Route
-          path="/organization"
-          element={<Organization />}
-        />
-
-        <Route
-          path="/template"
-          element={<Template />}
-        />
-
-        <Route
-          path="/workshop"
-          element={<Workshop />}
-        />
-      </Routes>
-    </HashRouter>
+    <>
+      
+      <Dashboard />
+    </>
   );
 }
 
