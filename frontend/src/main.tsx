c4cf163d69d0fb8fd761ab0947763bde260dbf50
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-28 13:24:39 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,16 +11,10 @@ import "./index.css";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
-msalInstance.initialize().then(() => {
-  msalInstance.handleRedirectPromise().then(() => {
-    ReactDOM.createRoot(
-      document.getElementById("root") as HTMLElement
-    ).render(
-      <React.StrictMode>
-        <MsalProvider instance={msalInstance}>
-          <App />
-        </MsalProvider>
-      </React.StrictMode>
-    );
-  });
-});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>
+);
