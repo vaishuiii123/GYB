1224454a9bf2244b4a319a9c222d570c886c1ba9
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-28 19:32:29 +0530

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,34 @@
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+
 export default function Dashboard() {
-  return <h1>Dashboard Working</h1>;
+  return (
+    <div
+      style={{
+        background: "#f3f4f6",
+        minHeight: "100vh",
+      }}
+    >
+      <Header />
+      <Sidebar />
+
+      <div
+        style={{
+          marginLeft: "250px",
+          paddingTop: "90px",
+          paddingLeft: "30px",
+          paddingRight: "30px",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "40px",
+            fontWeight: "700",
+          }}
+        >
+          Dashboard
+        </h1>
+      </div>
+    </div>
+  );
 }
