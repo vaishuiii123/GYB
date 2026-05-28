5a1ecbd5a7c0e6ac68fb03976805e76beba2c8e0
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-28 15:22:51 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
-
 import { msalConfig } from "./authConfig";
-
-import "./index.css";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
