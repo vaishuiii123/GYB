daefce7b153d5ee6f5b860dc6fcb597cb16323fb
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-28 15:29:06 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import { msalConfig } from "./authConfig";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <MsalProvider instance={msalInstance}>
       <App />
