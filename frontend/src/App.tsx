15d9cca1a320417fce6e930c94bd77d181e78eba
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-28 17:40:29 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,67 +1,23 @@
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "./authConfig";
 
-import {
-  HashRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Organization from "./pages/Organization";
-import Template from "./pages/Template";
-import Workshop from "./pages/Workshop";
 
 function App() {
   const { instance, accounts } = useMsal();
 
   const handleLogin = async () => {
-    try {
-      await instance.loginRedirect({
-        ...loginRequest,
-        prompt: "select_account",
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    await instance.loginRedirect({
+      ...loginRequest,
+      prompt: "select_account",
+    });
   };
 
   if (accounts.length === 0) {
     return <Login onLogin={handleLogin} />;
   }
 
-  return (
-    <HashRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate to="/dashboard" />}
-        />
-
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-
-        <Route
-          path="/organization"
-          element={<Organization />}
-        />
-
-        <Route
-          path="/template"
-          element={<Template />}
-        />
-
-        <Route
-          path="/workshop"
-          element={<Workshop />}
-        />
-      </Routes>
-    </HashRouter>
-  );
+  return <h1>Dashboard Working</h1>;
 }
 
 export default App;
