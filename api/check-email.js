f1355c5b1ed7cc19bd9cfa38758501204ccb70b9
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-05-29 21:09:35 +0530

    Update check-email.js

--- api/check-email.js
+++ api/check-email.js
@@ -1,12 +1,41 @@
+import { TableClient } from "@azure/data-tables";
+
 export async function GET() {
-  return new Response(
-    JSON.stringify({
-      message: "API Working"
-    }),
-    {
-      headers: {
-        "Content-Type": "application/json"
-      }
+  try {
+    const client = TableClient.fromConnectionString(
+      process.env.AZURE_STORAGE_CONNECTION_STRING,
+      "Users"
+    );
+
+    const users = [];
+
+    for await (const entity of client.listEntities()) {
+      users.push(entity);
     }
-  );
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        count: users.length,
+        users
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: error.message
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  }
 }
