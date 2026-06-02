6f345d0cd2924b50eaaeff3938e7642a9c5561c4
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-02 12:00:26 +0530

    Update index.js

--- api/check-email/index.js
+++ api/check-email/index.js
@@ -7,7 +7,7 @@ module.exports = async function (context, req) {
 
     const client = TableClient.fromConnectionString(
       process.env.AZURE_STORAGE_CONNECTION_STRING,
-      "AdminMailID"
+      "User"
     );
 
    const entities = client.listEntities({
