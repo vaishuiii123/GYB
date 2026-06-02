59d7e15528d497dd1ad49daa40153492590c6562
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-02 16:38:36 +0530

    Update index.js

--- api/create-organization/index.js
+++ api/create-organization/index.js
@@ -1,31 +1,43 @@
 const { TableClient } = require("@azure/data-tables");
 
 module.exports = async function (context, req) {
-  try {
+  try { 
     const {
-      organizationName,
-      contactPerson,
-      email,
-    } = req.body;
+        organizationName,
+        contactPerson,
+        email,
+        createdBy,
+      } = req.body;
+
+
+if (!organizationName || !contactPerson || !email) {
+      return {
+        status: 400,
+        body: {
+          success: false,
+          message: "All fields are required",
+        },
+      };
+    }
 
     const client =
       TableClient.fromConnectionString(
         process.env.AZURE_STORAGE_CONNECTION_STRING,
         "Organization"
       );
+    
+const rowKey = Date.now().toString();
 
+    
     await client.createEntity({
-      partitionKey: "Organisation_name",
-      rowKey: Date.now().toString(),
-
-      Organisation_Name:
-        organizationName,
-
-      Contact_Person:
-        contactPerson,
-
+      partitionKey: "Organization",
+      rowKey: rowKey,
+      Organization_Name: organizationName,
+      Contact_Person: contactPerson,
       Email: email,
+      Created_By: createdBy || "",
     });
+
 
     return {
       status: 200,
