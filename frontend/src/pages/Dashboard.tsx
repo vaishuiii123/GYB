b2e4961c47b55ed06ffc3ada502605cf9fa4b6fc
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-02 15:02:23 +0530

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 
-export default function Dashboard() {
+export default function Dashboard({ user }: any) {
   return (
     <div
       style={{
