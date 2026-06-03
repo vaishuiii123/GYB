af09c5540eec95a5b57ea5c6b8488856f28b5d07
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-03 18:18:24 +0530

    Update index.js

--- api/get-organizations/index.js
+++ api/get-organizations/index.js
@@ -1,6 +1,8 @@
 const { TableClient } = require("@azure/data-tables");
 
 module.exports = async function (context, req) {
+  const createdBy = req.query.createdBy;
+  
   try {
     const client = TableClient.fromConnectionString(
       process.env.AZURE_STORAGE_CONNECTION_STRING,
@@ -12,14 +14,21 @@ module.exports = async function (context, req) {
     const organizations = [];
 
     for await (const entity of entities) {
-      organizations.push({
-        id: entity.rowKey,
-        organizationName: entity.Organization_Name || "",
-        contactPerson: entity.Contact_Person || "",
-        email: entity.Email || "",
-        createdBy: entity.Created_By || "",
-      });
-    }
+
+  if (
+    entity.Created_By &&
+    entity.Created_By.toLowerCase() === createdBy.toLowerCase()
+  ) {
+    organizations.push({
+      id: entity.rowKey,
+      organizationName: entity.Organization_Name || "",
+      contactPerson: entity.Contact_Person || "",
+      email: entity.Email || "",
+      createdBy: entity.Created_By || "",
+    });
+  }
+}
+    
 
     return {
       status: 200,
