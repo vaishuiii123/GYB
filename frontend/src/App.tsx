c4265f0257b812d7c5737b0ea04c41d077edb424
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-04 19:57:57 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,52 +4,18 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-
-import AdminLogin from "./pages/auth/AdminLogin";
 import Dashboard from "./pages/Admin/Dashboard";
-import Organization from "./pages/Admin/Organization";
-import Template from "./pages/Admin/Template";
-import Workshop from "./pages/Admin/Workshop";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState<any>(() => {
-    const savedUser = localStorage.getItem("user");
-    return savedUser ? JSON.parse(savedUser) : null;
-  });
-
-  const handleLogin = (userData: any) => {
-    localStorage.setItem("user", JSON.stringify(userData));
-    setCurrentUser(userData);
-  };
 
   return (
     <HashRouter>
       <Routes>
         <Route
-          path="/"
-          element={<AdminLogin onLogin={handleLogin} />}
-        />
-
-        <Route
           path="/dashboard"
-          element={<Dashboard user={currentUser} />}
+          element={<Dashboard/>}
         />
-
-        <Route
-          path="/organization"
-          element={<Organization user={currentUser} />}
-        />
-
-        <Route
-          path="/template"
-          element={<Template user={currentUser} />}
-        />
-
-        <Route
-          path="/workshop"
-          element={<Workshop user={currentUser} />}
-        />
-      </Routes>
+      </Routes>  
     </HashRouter>
   );
 }
