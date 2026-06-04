a612177ffba911e3ed91e46dd0a59373aa686620
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-04 19:14:01 +0530

    Update Template.tsx

--- frontend/src/pages/Admin/Template.tsx
+++ frontend/src/pages/Admin/Template.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
 
 type PageProps = {
   user?: any;
