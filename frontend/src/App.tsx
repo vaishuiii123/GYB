30790221a6e9d52d585c6a652c35f1ccd73341d0
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-04 20:12:09 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,21 @@ import {
   Route,
 } from "react-router-dom";
 import Dashboard from "./pages/Admin/Dashboard";
+import AdminLogin from "./pages/auth/AdminLogin";
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState<any>(() => {
+    const savedUser = localStorage.getItem("user");
+
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  const handleLogin = (userData: any) => {
+    localStorage.setItem("user", JSON.stringify(userData));
+    setCurrentUser(userData);
+  };
+
 
   return (
     <HashRouter>
