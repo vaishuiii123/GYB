2d50b7c95a45e3ca1a23e8176f37e05722ec3041
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-08 17:57:16 +0530

    Create index.js

---
+++ api/create-master-category/index.js
@@ -0,0 +1,35 @@
+const { TableClient } = require("@azure/data-tables");
+
+module.exports = async function (context, req) {
+  try {
+    const { masterCategoryName, createdBy } = req.body;
+
+    const client =
+      TableClient.fromConnectionString(
+        process.env.AZURE_STORAGE_CONNECTION_STRING,
+        "MasterCategory"
+      );
+
+    await client.createEntity({
+      partitionKey: "MasterCategory",
+      rowKey: Date.now().toString(),
+      MasterCategoryName: masterCategoryName,
+      Created_By: createdBy,
+    });
+
+    context.res = {
+      status: 200,
+      body: {
+        success: true,
+      },
+    };
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: {
+        success: false,
+        error: error.message,
+      },
+    };
+  }
+};
