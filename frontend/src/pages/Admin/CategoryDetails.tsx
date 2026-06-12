47fb6de5f100083f8107d67e546c20a616424755
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-12 16:57:49 +0530

    Update CategoryDetails.tsx

--- frontend/src/pages/Admin/CategoryDetails.tsx
+++ frontend/src/pages/Admin/CategoryDetails.tsx
@@ -173,6 +173,7 @@ export default function CategoryDetails({
                       },
                     }
                   )
+                }
               >
                 Master Categories
               </span>
