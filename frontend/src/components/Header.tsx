cfb5fda82f4a29335e8ba01899b5f134b155c084
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-17 21:48:19 +0530

    Update Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../images/KNAV logo.png";
 
 export default function Header({ user }: any) {
 
@@ -7,10 +6,9 @@ export default function Header({ user }: any) {
 
   const handleLogout = () => {
 
-    const confirmLogout =
-      window.confirm(
-        "Do you really want to logout?"
-      );
+    const confirmLogout = window.confirm(
+      "Do you really want to logout?"
+    );
 
     if (confirmLogout) {
 
@@ -24,9 +22,7 @@ export default function Header({ user }: any) {
   const initials =
     user?.name
       ?.split(" ")
-      .map(
-        (word: string) => word[0]
-      )
+      .map((word: string) => word[0])
       .join("")
       .toUpperCase() || "U";
 
@@ -36,41 +32,26 @@ export default function Header({ user }: any) {
       style={{
         position: "fixed",
         top: 0,
-        left: "220px",
+        left: "260px", // same as sidebar width
         right: 0,
         height: "70px",
+
         background: "#741D34",
+
         display: "flex",
+        justifyContent: "flex-end",
         alignItems: "center",
-        justifyContent: "space-between",
+
         padding: "0 30px",
+
         boxSizing: "border-box",
+
         zIndex: 1000,
 
         boxShadow:
           "0 4px 12px rgba(0,0,0,0.12)",
       }}
     >
-
-      {/* Logo */}
-
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <img
-          src={logo}
-          alt="KNAV Logo"
-          style={{
-            height: "40px",
-            objectFit: "contain",
-          }}
-        />
-      </div>
-
-      {/* Right Section */}
 
       <div
         style={{
@@ -80,35 +61,40 @@ export default function Header({ user }: any) {
         }}
       >
 
-        {/* User Avatar */}
+        {/* User Circle */}
 
         <div
           style={{
             width: "42px",
             height: "42px",
+
             borderRadius: "50%",
+
             background: "#FCECEF",
+
             color: "#741D34",
-            border:
-              "1px solid rgba(255,255,255,0.25)",
 
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
 
             fontWeight: 700,
-            fontSize: "14px",
+            fontSize: "15px",
+
+            border:
+              "1px solid rgba(255,255,255,0.25)",
           }}
         >
           {initials}
         </div>
 
-        {/* Separator */}
+        {/* Divider */}
 
         <div
           style={{
             width: "1px",
             height: "32px",
+
             background:
               "rgba(255,255,255,0.30)",
           }}
@@ -121,12 +107,17 @@ export default function Header({ user }: any) {
           style={{
             display: "flex",
             alignItems: "center",
+
             gap: "8px",
+
             cursor: "pointer",
 
             color: "#FFFFFF",
+
             fontWeight: 600,
+
             fontSize: "15px",
+
             userSelect: "none",
           }}
         >
