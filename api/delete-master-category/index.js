4eceb16d4ea800bd39d3c2119309677f5d9e657d
vaishuiii123 <vaishnavi.sakpal@knavus.com>
2026-06-17 19:07:55 +0530

    Create index.js

---
+++ api/delete-master-category/index.js
@@ -0,0 +1,50 @@
+const { TableClient } = require("@azure/data-tables");
+
+module.exports = async function (context, req) {
+
+  try {
+
+    const { id } = req.body;
+
+    if (!id) {
+
+      context.res = {
+        status: 400,
+        body: {
+          success: false,
+          message: "Id is required"
+        }
+      };
+
+      return;
+    }
+
+    const client =
+      TableClient.fromConnectionString(
+        process.env.AZURE_STORAGE_CONNECTION_STRING,
+        "Questionnairemastercategory"
+      );
+
+    await client.deleteEntity(
+      "MasterCategory",
+      id
+    );
+
+    context.res = {
+      status: 200,
+      body: {
+        success: true
+      }
+    };
+
+  } catch (error) {
+
+    context.res = {
+      status: 500,
+      body: {
+        success: false,
+        error: error.message
+      }
+    };
+  }
+};
