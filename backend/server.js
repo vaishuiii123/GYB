a9e3ca8882905bbd35527708854397e616b47e9d
Vaishnavi Sakpal <vaishnavi.sakpal@knavus.com>
2026-07-28 13:33:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const { TableClient } = require("@azure/data-tables");
 
 const participantsTable = TableClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING,"Participants");
 const workshopsTable = TableClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING,"Workshops");
-const organizationsTable = TableClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING,"Organizations");
+const organizationsTable = TableClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING,"organizations");
 
 app.post("/api/gyb/login", async (req, res) => {
     try {
@@ -44,6 +44,43 @@ app.post("/api/gyb/login", async (req, res) => {
         });
     } catch (error) {
         console.error(error);
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+});
+
+//**************************** GET ORGANIZATION
+
+app.get("/api/gyb/organizations", async (req, res) => {
+    try {
+        const { createdBy } = req.query;
+        const organizations = [];
+        for await (const entity of organizationsTable.listEntities()) {
+            if (
+                entity.Created_By &&
+                createdBy &&
+                entity.Created_By.toLowerCase() === createdBy.toLowerCase()
+            ) {
+                organizations.push({
+                    id: entity.rowKey,
+                    organizationName: entity.Organization_Name || "",
+                    contactPerson: entity.Contact_Person || "",
+                    email: entity.Email || "",
+                    createdBy: entity.Created_By || "",
+                });
+            }
+        }
+
+        res.status(200).json({
+            success: true,
+            organizations
+        });
+
+    } catch (error) {
+        console.error(error);
+
         res.status(500).json({
             success: false,
             error: error.message
